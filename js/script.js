2a6fd659fc7aa3f0d89386c3f5ccc7c622aae7ca
Nsilvaa <nsilvadesouza23@gmail.com>
2026-04-29 17:02:14 -0300

    atividade15

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ function mensagem(){
 function confirmar(){
     return window.confirm("Realmente deseja ir para outra página?");
 }
+function excluir(){
+    window.alert("Tem certeza que deseja excluir esse formulário?")
+}
